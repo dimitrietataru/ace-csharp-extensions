e0355c7289e7b841a2b0479bdcad9d5959a9063d
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-07-20 18:28:14 +0300

    Add String.Format Local/Invariant extensions

---
+++ src/Ace.CSharp.Extensions/StringExtensions/StringExtensions.FormatInvariant.cs
@@ -0,0 +1,24 @@
+namespace Ace.CSharp.Extensions;
+
+public static partial class StringExtensions
+{
+    public static string FormatInvariant(this string format, object? arg0)
+    {
+        return string.Format(CultureInfo.InvariantCulture, format, arg0);
+    }
+
+    public static string FormatInvariant(this string format, object? arg0, object? arg1)
+    {
+        return string.Format(CultureInfo.InvariantCulture, format, arg0, arg1);
+    }
+
+    public static string FormatInvariant(this string format, object? arg0, object? arg1, object? arg2)
+    {
+        return string.Format(CultureInfo.InvariantCulture, format, arg0, arg1, arg2);
+    }
+
+    public static string FormatInvariant(this string format, object?[] args)
+    {
+        return string.Format(CultureInfo.InvariantCulture, format, args);
+    }
+}
