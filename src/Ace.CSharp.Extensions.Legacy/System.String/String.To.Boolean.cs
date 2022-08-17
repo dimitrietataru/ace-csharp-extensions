097d5c44dd3125286c4e1ffc9473a86bb997392e
Dimitrie Tataru <dimitrie.tataru.bp@emea.nttdata.com>
2022-08-17 19:54:01 +0300

    Test release

---
+++ src/Ace.CSharp.Extensions.Legacy/System.String/String.To.Boolean.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace Ace.CSharp.Extensions
+{
+    public static partial class StringExtensions
+    {
+        public static bool ToBoolean(this string @this, IFormatProvider provider)
+        {
+            return Convert.ToBoolean(@this, provider);
+        }
+
+        public static bool ToBooleanOrDefault(this string @this, IFormatProvider provider, bool @default = default)
+        {
+            bool isBoolean = TryConvertToBoolean(@this, provider, out bool result);
+
+            return isBoolean ? result : @default;
+        }
+
+        public static bool TryConvertToBoolean(this string @this, IFormatProvider provider, out bool result)
+        {
+            try
+            {
+                result = Convert.ToBoolean(@this, provider);
+
+                return true;
+            }
+            catch (FormatException)
+            {
+                result = default;
+
+                return false;
+            }
+        }
+    }
+}
