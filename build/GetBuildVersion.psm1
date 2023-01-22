668bf49e8d89b6997c6b0977f545f23c2aa77b7a
Dimitrie Tataru <dimitriettr@gmail.com>
2023-01-23 00:24:16 +0200

    Improve the 'GetBuildVersion' script

--- build/GetBuildVersion.psm1
+++ build/GetBuildVersion.psm1
@@ -3,25 +3,20 @@ Function GetBuildVersion {
         [string]$VersionString
     )
 
-    # Process through regex
     $VersionString -match "(?<major>\d+)(\.(?<minor>\d+))?(\.(?<patch>\d+))?(\-(?<pre>[0-9A-Za-z\-\.]+))?(\+(?<build>\d+))?" | Out-Null
-
     if ($matches -eq $null) {
         return "1.0.0-build"
     }
 
-    # Extract the build metadata
-    $BuildRevision = [uint64]$matches['build']
-    # Extract the pre-release tag
-    $PreReleaseTag = [string]$matches['pre']
-    # Extract the patch
-    $Patch = [uint64]$matches['patch']
-    # Extract the minor
-    $Minor = [uint64]$matches['minor']
-    # Extract the major
-    $Major = [uint64]$matches['major']
-
-    $Version = [string]$Major + '.' + [string]$Minor + '.' + [string]$Patch;
+	$Major = [uint64]$matches['major']
+	$Minor = [uint64]$matches['minor']
+	$Patch = [uint64]$matches['patch']
+	
+	$PreReleaseTag = [string]$matches['pre']
+	$BuildRevision = [uint64]$matches['build']
+	
+	$Version = [string]$Major + '.' + [string]$Minor + '.' + [string]$Patch;
+	
     if ($PreReleaseTag -ne [string]::Empty) {
         $Version = $Version + '-' + $PreReleaseTag
     }
