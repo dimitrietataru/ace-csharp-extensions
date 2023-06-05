17ef817b9766a4df917b23c301e0042b7c9f95cc
MYT <dimitriettr@gmail.com>
2023-06-05 12:38:27 +0300

    Add 'clean-solution' bat

---
+++ src/clean-solution.bat
@@ -0,0 +1,10 @@
+rmdir /s /q .\Ace.CSharp.Extensions\bin
+rmdir /s /q .\Ace.CSharp.Extensions\obj
+
+rmdir /s /q .\Ace.CSharp.Extensions.Legacy\bin
+rmdir /s /q .\Ace.CSharp.Extensions.Legacy\obj
+
+rmdir /s /q .\Ace.CSharp.Extensions.Tests\bin
+rmdir /s /q .\Ace.CSharp.Extensions.Tests\obj
+
+pause
